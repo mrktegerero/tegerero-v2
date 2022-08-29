9db3ea40c25fbd8b34c38ac937bf72d4390ece8c
Kurt Tegerero <kurt.tegerero@sodadigital.com.au>
2022-08-29 17:11:08 +0800

    install svelte

---
+++ src/routes/+page.svelte
@@ -0,0 +1,2 @@
+<h1>Kurt Tegerero</h1>
+<p>Front End Developer <a href="https://www.sodadigital.com.au/">@sodadigital</a></p>
---
+++ svelte.config.js
@@ -0,0 +1,46 @@
+import preprocess from 'svelte-preprocess';
+import * as path from 'node:path';
+
+import { fileURLToPath } from 'url';
+// import adapter from '@sveltejs/adapter-cloudflare-workers';
+import adapter from '@svelte/adapter-cloudflare-workers';
+
+const __filename = fileURLToPath(import.meta.url);
+
+const __dirname = path.dirname(__filename);
+console.log(path.resolve(__dirname));
+/** @type {import('@sveltejs/kit').Config} */
+const config = {
+	// Consult https://github.com/sveltejs/svelte-preprocess
+	// for more information about preprocessors
+	preprocess: [
+		preprocess({
+			typescript: true,
+			postcss: {
+				configFilePath: path.resolve(__dirname, './postcss.config.cjs')
+			}
+		})
+	],
+	vitePlugin: {
+		experimental: {
+			inspector: true
+		}
+	},
+	kit: {
+		version: {
+			pollInterval: 60000
+		},
+		prerender: {
+			enabled: false
+		},
+		//inlineStyleThreshold: 1024,
+		/**
+		 * {
+			external: ['crypto']
+		}
+		 */
+		adapter: adapter()
+	}
+};
+
+export default config;
